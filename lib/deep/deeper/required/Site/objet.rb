798adf1466e903ca57f379f4f4bf277a2fcbe6b9
Philippe Perret <philippe.perret@yahoo.fr>
2016-01-12 09:07:43 +0100

    Requérir aussi css et js avec require_objet

--- lib/deep/deeper/required/Site/objet.rb
+++ lib/deep/deeper/required/Site/objet.rb
@@ -12,10 +12,15 @@ class SiteHtml
   # Requiert tout le dossier lib/required de l'objet de nom
   # +objet_name+. +objet_name+ doit être un nom contenu dans le
   # dossier `./objet`
+  # Charge également tous les CSS et tous les JS du dossier
+  # lib/required
   # Note : Pour le moment, produit une erreur fatale si le dossier
   # n'existe pas.
   def require_objet objet_name
-    (site.folder_objet + "#{objet_name}/lib/required").require
+    dossier = site.folder_objet + "#{objet_name}/lib/required"
+    dossier.require
+    page.add_css        Dir["#{dossier}/**/*.css"]
+    page.add_javascript Dir["#{dossier}/**/*.js"]
   end
 
 end
