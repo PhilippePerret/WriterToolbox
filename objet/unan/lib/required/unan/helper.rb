7c554931c74c46d28a8f814c756d39d6c1172714
Philippe Perret <philippe.perret@yahoo.fr>
2016-01-13 22:52:05 +0100

    Ajout d'un lien bureau

--- objet/unan/lib/required/unan/helper.rb
+++ objet/unan/lib/required/unan/helper.rb
@@ -14,37 +14,46 @@ class Unan
     def view path
       Vue::new(path, Unan::folder, Unan).output
     end
-  end # << self
-class Helper
-  class << self
 
-    # Construit le code d'une graduation pour les
-    # jour dans un div. Cf. le manuel Vues > Graduations-jours.md
-    def graduation_jours data
-      from_day  = data.delete( :from )  || 1
-      to_day    = data.delete( :to )    || 365
-      count_days = to_day - from_day
-
-      # Les données pour les styles dynamiques de la
-      # règle dont aura besoin `graduation_jours_styles`
-      data[:day_width]        -= 1
-      data[:padding_left]     ||= 12
-      data[:background_color] ||= "#FFC740"
-      data[:color]            ||= "#808080"
-      data[:width]            = (data[:day_width] * (count_days+1)) + data[:padding_left]
-
-      graduation_jours_styles(data) +
-      (count_days + 1).times.collect do |itime|
-        (itime+1).to_s.in_span.in_li
-      end.join.in_ul(class:'dayruler')
+    # Return un lien HTML pour rejoindre le bureau
+    def lien_bureau titre = "bureau", options = nil
+      options ||= Hash::new
+      options.merge!( href: "bureau/home?in=unan" )
+      titre.in_a( options )
     end
-    # Les styles dynamiques pour la règle
-    def graduation_jours_styles data
-      "<style type='text/css'>"+
-        "ul.dayruler{background-color:#{data[:background_color]};padding-left:#{data[:padding_left]}px;width:#{data[:width]}}" +
-        "ul.dayruler>li{color:#{data[:color]};width:#{data[:day_width]}px}"+
-      "</style>"
-    end
+
   end # << self
-end #/Helper
+
+  class Helper
+    class << self
+
+      # Construit le code d'une graduation pour les
+      # jour dans un div. Cf. le manuel Vues > Graduations-jours.md
+      def graduation_jours data
+        from_day  = data.delete( :from )  || 1
+        to_day    = data.delete( :to )    || 365
+        count_days = to_day - from_day
+
+        # Les données pour les styles dynamiques de la
+        # règle dont aura besoin `graduation_jours_styles`
+        data[:day_width]        -= 1
+        data[:padding_left]     ||= 12
+        data[:background_color] ||= "#FFC740"
+        data[:color]            ||= "#808080"
+        data[:width]            = (data[:day_width] * (count_days+1)) + data[:padding_left]
+
+        graduation_jours_styles(data) +
+        (count_days + 1).times.collect do |itime|
+          (itime+1).to_s.in_span.in_li
+        end.join.in_ul(class:'dayruler')
+      end
+      # Les styles dynamiques pour la règle
+      def graduation_jours_styles data
+        "<style type='text/css'>"+
+          "ul.dayruler{background-color:#{data[:background_color]};padding-left:#{data[:padding_left]}px;width:#{data[:width]}}" +
+          "ul.dayruler>li{color:#{data[:color]};width:#{data[:day_width]}px}"+
+        "</style>"
+      end
+    end # << self
+  end #/Helper
 end #/Unan
