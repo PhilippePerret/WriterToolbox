d3461bff68c1c88d127e2bd47badfeec6e02c5fd
Philippe Perret <philippe.perret@yahoo.fr>
2016-01-16 15:37:06 +0100

    Minor changes

--- CRON/lib/required.rb
+++ CRON/lib/required.rb
@@ -6,8 +6,6 @@ Tout ce qu'il y a à requérir pour le cron
 =end
 log "-> lib/required.rb"
 
-APP_FOLDER  = File.expand_path( File.dirname(THIS_FOLDER) )
-
 # Requérir tout le dossier ./lib/required
 Dir["#{THIS_FOLDER}/lib/required/**/*.rb"].each{|m| require m}
 
---
+++ CRON/lib/required/__constantes__.rb
@@ -0,0 +1,5 @@
+# encoding: UTF-8
+
+APP_FOLDER  = File.expand_path( File.dirname(THIS_FOLDER) )
+ONLINE      = true
+OFFLINE     = false
