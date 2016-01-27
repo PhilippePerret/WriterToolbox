e7e31ee3f750c594b5ba023f3001246e35b690ca
Philippe Perret <philippe.perret@yahoo.fr>
2016-01-27 13:35:24 +0100

    Amélioration accueil

--- objet/forum/lib/required/forum/helper.rb
+++ objet/forum/lib/required/forum/helper.rb
@@ -5,4 +5,18 @@ Méthodes d'helper pour le forum
 class Forum
   # Rappel : C'est un SINGLETON
 
+  # Pour écrire titre et sous-titre
+  # @usage    <%= forum.titre_h1[("<sous titre>")] %>
+  def titre_h1 sous_titre = nil
+    t = "Forum".in_h1
+    t << sous_titre.in_h2 unless sous_titre.nil?
+    t
+  end
+
+  # Pour afficher les onglets généraux
+  # @usage:  <%= forum.onglets %>
+  def onglets
+    "[ONGLETS]"
+  end
+
 end
