6fb14736c9f82b72e73387bdb64ca5af821d4949
Philippe Perret <philippe.perret@yahoo.fr>
2016-02-14 19:32:33 +0100

    Amélioration structure calcul

--- objet/calculateur/main.js
+++ objet/calculateur/main.js
@@ -22,10 +22,15 @@ $.extend(window.STT, {
 
 		var duree_film = centaines + dizaines + unites ;
 		if ( duree_film == 0 ) {
-			if( ! DepuisMenuChangeUnite )
-				alert("Il faut définir la durée du film ! (dans la première ligne de la table)") ;
+			F.error("Il faut définir la durée du film ! (dans la première ligne de la table)") ;
 			return ;
 		}
+
+		// Tous les span de class 'unite' doivent être renseignés
+		// avec la valeur de l'unité
+		$('span.unite').html(ParPages ? 'page' : '')
+		$('span.unite_a').html(ParPages ? 'page' : 'à')
+
 		/* Douzième et demi douzième */
 		var douzieme 			= duree_film / 12 ;
 		var demi_douzieme = douzieme / 2 ;
@@ -42,17 +47,17 @@ $.extend(window.STT, {
 		LParties['pvt1']				= pvt1 ;
 		LParties['pvt1_start']			= do_end ;
 		LParties['pvt1_end']			= pvt1 ;
-		LParties['expo_start']		= ParPages ? "page 1" : "0:00:00"
-		LParties['expo_end']			= ParPages ? "page " + parseInt(pvt1) : pvt1 ;
+		LParties['expo_start']		= ParPages ? "1" : 0
+		LParties['expo_end']			= ParPages ? parseInt(pvt1) : pvt1 ;
 		var pvt2 = Math.round(douzieme * 9 * 10 ) / 10 ;
 		var pvt2_start = Math.round(douzieme * 8 * 10 ) / 10 ;
-		LParties['dev_start']			= ParPages ? "page " + parseInt(pvt1) : pvt1 ;
-		LParties['dev_end']				= ParPages ? "page " + parseInt(pvt2) : pvt2 ;
+		LParties['dev_start']			= ParPages ? parseInt(pvt1) : pvt1 ;
+		LParties['dev_end']				= ParPages ? parseInt(pvt2) : pvt2 ;
 		LParties['pvt2']					= pvt2 ;
 		LParties['pvt2_start']		= pvt2_start ;
 		LParties['pvt2_end']			= pvt2 ;
-		LParties['den_start']			= ParPages ? "page " + parseInt(pvt2) : pvt2 ;
-		LParties['den_end']				= ParPages ? "page " + parseInt(duree_film) : duree_film ;
+		LParties['den_start']			= ParPages ? parseInt(pvt2) : pvt2 ;
+		LParties['den_end']				= ParPages ? parseInt(duree_film) : duree_film ;
 		var climax_start = Math.round(douzieme * 11 * 10 ) / 10 ;
 		LParties['climax_start']		= climax_start ;
 		var cdv = (duree_film / 2) ;
@@ -65,28 +70,13 @@ $.extend(window.STT, {
 		/*= ON MET LES VALEURS DANS LES SPAN =*/
 		for( var oid in LParties){
 			var value = LParties[oid] ;
-			if( ParPages ){
-				if( value.toString().indexOf('page') < 0 ){ value = "p. " + parseInt(value) }
-			} else {
-				value = Time.s2h(Math.round(60*value))
-			}
+			if( ParPages ){ value = parseInt(value) }
+			else { value = Time.s2h(Math.round(60*value)) }
 			$('label#'+oid).html( value )
 		}
-		$('span#expo_start').html(ParPages ? "page 1" : "0:00:00");
 
 	},
 
-	on_change_minutes_or_page:function(unite){
-		var entre, etpage ;
-		if(undefined == unite){unite = $('select#pages_ou_minutes').val()}
-		if(unite == 'pages'){
-			entre = "Entre page…" ; etpage = "et page…" ;
-		} else {
-			entre = "Entre minute…" ; etpage = "et minute…" ;
-		}
-		$('td#td_entre_page').html(entre);
-		$('td#td_et_page').html(etpage);
-	}
 })
 
 function STT_CalculStructureIdeale(DepuisMenuChangeUnite) {
@@ -94,7 +84,5 @@ function STT_CalculStructureIdeale(DepuisMenuChangeUnite) {
 
 
 $(document).ready(function(){
-	// Pour réajuster le texte Entre page… / Entre minute suivant
-	// l'unité choisie
-	STT.on_change_minutes_or_page() ;
+
 })
