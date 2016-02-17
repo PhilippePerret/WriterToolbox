ea599790c721431ea6f382e622d7572f7d443577
Philippe Perret <philippe.perret@yahoo.fr>
2016-02-17 10:09:36 +0100

    Proposition de mot OK

--- lib/deep/deeper/required/Site/mail.rb
+++ lib/deep/deeper/required/Site/mail.rb
@@ -1,11 +1,25 @@
 # encoding: UTF-8
 class SiteHtml
 
+  # @usage:   site.send_mail(data_mail)
   # Cf. optional/Site/mail.rb pour le détail
   def send_mail data_mail
     # puts "J'envoie le mail à #{data_mail[:to]}"
     app.require_optional 'Site/mail'
     exec_send_mail data_mail
+  end
+
+  # Envoi d'un mail à l'administration par
+  # l'user courant
+  # +data_mail+ a juste à définir :
+  #   :message
+  #   :subject
+  def send_mail_to_admin data_mail
+    data_mail.merge!(
+      from: user.mail,
+      to:   site.mail
+    )
+    send_mail data_mail
   end
 
 end
--- objet/scenodico/proposer.rb
+++ objet/scenodico/proposer.rb
@@ -3,15 +3,33 @@ class Scenodico
   class << self
 
     attr_reader :suggested_mot
+    attr_reader :proposition_ok
 
     def proposer_mot
       @suggested_mot = param(:newmot)[:mot].nil_if_empty
       raise "Il faut donner le mot à proposer !" if @suggested_mot.nil?
       check_existence_mot || return
-
+      # Le mot est correct, on peut le proposer
+      site.send_mail_to_admin(
+        subject:    "Proposition d'un mot pour le scénodico",
+        message:    message_proposition_mot,
+        formated:  true
+      )
+      # Message de remerciement
+      @proposition_ok = true
     rescue Exception => e
       debug e
       error e.message
+    end
+
+    def message_proposition_mot
+      <<-HTML
+<p>Bonjour administrateur,</p>
+<p>#{user.pseudo} propose d'ajouter le mot suivant au Scénodico :</p>
+<p class='bold'>#{suggested_mot}</p>
+<p>Pour l'ajouter, il suffit de se rendre à l'adresse : </p>
+<p>#{site.distant_url}/scenodico/edit</p>
+      HTML
     end
 
     def check_existence_mot
