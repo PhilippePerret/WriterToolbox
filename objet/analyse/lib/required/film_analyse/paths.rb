1be17d9977e2013aa93dcba7bf1a57afc2137e01
Philippe Perret <philippe.perret@yahoo.fr>
2016-02-26 14:01:29 +0100

    Some stuff

--- objet/analyse/lib/required/film_analyse/paths.rb
+++ objet/analyse/lib/required/film_analyse/paths.rb
@@ -7,10 +7,23 @@ class << self
   end
 
 
-  # Dossier contenant les analyses
+  # Dossier contenant les analyses des films TM (TextMate)
+  # Voir aussi le dossier `folder_per_film` qui contient les
+  # données par film.
   def folder_films
     @folder_analyses ||= begin
-      d = site.folder_data+'analyse/films'
+      d = site.folder_data+'analyse/films_tm'
+      d.build unless d.exist?
+      d
+    end
+  end
+
+  # Dossier principal d'analyse qui contient les données des
+  # films classées par film. Dossier initié pour contenir les
+  # évènemenciers récupérés.
+  def folder_per_film
+    @folder_per_film ||= begin
+      d = site.folder_data+'analyse/data_per_film'
       d.build unless d.exist?
       d
     end
