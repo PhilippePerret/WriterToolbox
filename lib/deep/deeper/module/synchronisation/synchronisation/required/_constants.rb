dd9125a5f17fc2b6cc26795218bfd944887d8829
Philippe Perret <philippe.perret@yahoo.fr>
2016-02-27 17:06:23 +0100

    Synchronisation des fichiers Marshal

--- lib/deep/deeper/module/synchronisation/synchronisation/required/_constants.rb
+++ lib/deep/deeper/module/synchronisation/synchronisation/required/_constants.rb
@@ -1,6 +1,6 @@
 # encoding: UTF-8
 #
 # Extensions communes
-COMMON_EXTENSIONS = ['rb', 'erb', 'js', 'css', 'txt', 'md', 'jpg', 'png']
+COMMON_EXTENSIONS = ['rb', 'erb', 'js', 'css', 'txt', 'md', 'jpg', 'png', 'msh']
 
 FOLDER_SYNCHRONISATION_RELPATH = FOLDER_SYNCHRONISATION.sub(/^#{File.expand_path('.')}/, '.')
