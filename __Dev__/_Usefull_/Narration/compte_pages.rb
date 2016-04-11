f460b9d907110a8c35fd12d1bf84a89647cf0b2a
Philippe Perret <philippe.perret@yahoo.fr>
2016-04-11 23:04:31 +0200

    Some stuff

---
+++ __Dev__/_Usefull_/Narration/compte_pages.rb
@@ -0,0 +1,18 @@
+# encoding: UTF-8
+=begin
+
+@usage :  CMD + i
+
+Ce module permet de comptabiliser le nombre de pages réelles
+dans la collection Narration de façon indépendante du programme
+pour être sûr que les calculs sont justes (en attendant d'être
+certain que le calcul dans l'état des lieux est juste).
+
+L'idée est de lancer ce script, de lancer la commande
+`inventory narration` et de comparer tous les résultats.
+
+=end
+
+puts <<-HTML
+Titre
+HTML
