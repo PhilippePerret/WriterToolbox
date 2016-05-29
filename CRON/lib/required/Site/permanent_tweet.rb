2f5ba42a3e65bff30e65f058ffd025f2f3376fa7
Philippe Perret <philippe.perret@yahoo.fr>
2016-05-29 21:40:50 +0200

    Minor change

--- CRON/lib/required/Site/permanent_tweet.rb
+++ CRON/lib/required/Site/permanent_tweet.rb
@@ -16,10 +16,10 @@ class Tweet
       if ok
         safed_log "  = #{message_retour}"
       else
-        error_log "  ### PROBLÈME ENVOI DES TWEETS : #{message_retour}"
+        error_log "  ### [#{Time.now}] PROBLÈME ENVOI DES TWEETS : #{message_retour}"
       end
     rescue Exception => e
-      error_log e, "# Problème en envoyant les tweets permanents"
+      error_log e, "# [#{Time.now}] Problème en envoyant les tweets permanents"
     else
       safed_log "  = Tweets permanents envoyés avec succès."
     end
@@ -28,8 +28,37 @@ class Tweet
     # Retourne TRUE si on doit envoyer un tweet
     # en fonction de la fréquence d'envoi déterminée par la
     # constante PERMANENT_TWEET_FREQUENCE
+    #
+    # On prend la date du dernier tweet envoyé
     def time_ok_to_send
-      (Time.now.hour % PERMANENT_TWEET_FREQUENCE) == 0
+      if last_sent.nil?
+        (Time.now.hour % PERMANENT_TWEET_FREQUENCE) == 0
+      else
+        Time.now.to_i > last_sent + (PERMANENT_TWEET_FREQUENCE * 3600)
+      end
+    end
+
+    def last_sent
+      @last_sent ||= begin
+        require 'sqlite3'
+        db = SQLite3::Database.new(db_site_cold)
+        rp = db.prepare request_last_sent
+        rp.execute.each_hash { |h| return h['last_sent'] }
+      end
+    rescue Exception => e
+      error_log e, "# [#{Time.now}] Problème en récupérant la date de dernier envoi."
+      nil
+    end
+    def request_last_sent
+      <<-SQL
+SELECT last_sent
+  FROM permanent_tweets
+  ORDER BY last_sent DESC
+  LIMIT 1
+      SQL
+    end
+    def db_site_cold
+      @db_site_cold ||= './database/data/site_cold.db'
     end
 
   end #<< self
