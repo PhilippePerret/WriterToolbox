6ff754ee77f9a58bd10eef836a8f2e640639b9ed
Philippe Perret <philippe.perret@yahoo.fr>
2016-07-07 13:15:10 +0200

    Some stuff (récupération de toutes les procédures)

--- CRON2/lib/procedure/send_mail_admin.rb
+++ CRON2/lib/procedure/send_mail_admin.rb
@@ -4,11 +4,55 @@
 
 class CRON2
 
-    SEND_MAIL = true
+
+    SEND_REPORT_TO_ADMIN = true 
 
     # La méthode principale qui envoie le mail final à l'administrateur
     # si c'est nécessaire, c'est-à-dire si la constante SEND_MAIL est à
     # true et/ou qu'il s'est produit des erreurs au cours du cron
     def send_mail_admin
+        # TODO Il faudra voir comment se définit ce nombre d'erreur en 
+        # sachant que s'il est ici, c'est bon, c'est une variable de
+        # l'instance singleton de CRON2 qui pourra être affectée par
+        # tous les autres processus.
+       @nombre_erreurs ||= 0
+        # En fait il faut envoyer le rapport seulement si la constante
+        # le demande et/ou s'il y a des erreurs. Donc si le nombre d
+        # 'erreurs est nil et qu'il ne faut pas envoyer le rapport, 
+        # alors on peut s'en retourner illico.
+        next if !SEND_REPORT_TO_ADMIN && @nombre_erreurs == 0
+        # Initialisation du rapport administrateur
+        areport = String.new
+        if File.exist?(logerrorpath)
+            mess =
+                if @nombre_erreurs > 1
+                    "#{@nombre_erreurs} erreurs se sont produites"
+                else
+                    'Une erreur s’est produite'
+                end
+            areport << "<strong class='warning' style='display:block;margin:2em;border:1px solid red;font-size:13pt'>#{mess} au cours du dernier cron LOCAL…</strong>\n"
+            areport << File.open(logerrorpath,'rb'){|f| f.read.force_encoding('utf-8')}
+        end
+
+        # On ajoute toujours le rapport normal s'il faut un rapport
+        # complet. Sinon, on indique juste un message simple
+        areport << File.open(logpath,'rb'){|f| f.read.force_encoding('utf-8')}
+
+        # On transforme les retours chariot en DIV et on définit la
+        # taille de la police.
+        areport = areport.
+            split("\n").
+            collect{|p|"<div>#{p}</div>"}.join('').
+            in_div(style: 'font-size: 12pt')
+
+        # On envoie le rapport
+        site.send_mail_to_admin(
+            subject:        "RAPPORT CRON LOCAL #{NOW.as_human_date(true, true, ' ')}",
+            message:        areport,
+            formated:       true,
+            force_offline:  true
+        )
+
     end
+
 end
--- CRON2/lib/procedure/stats_connexions/instance_report.rb
+++ CRON2/lib/procedure/stats_connexions/instance_report.rb
@@ -8,7 +8,7 @@ class CRON2
         def initialize
             @report = Hash.new
             @report = {
-                by_ip:    Hash::new,
+                by_ip:    Hash.new,
                 message:  nil,
                 titre:    nil
             }
@@ -20,6 +20,73 @@ class CRON2
             recolte_connexions_by_ip
             rationalise_connexions
             build_message_admin
+        end
+
+        # Envoi du rapport à l'administrateur:
+        def send_report
+            log "   -> Envoi du rapport de connexion à l'administration"
+            log "      Mail : #{site.mail}"
+            res = site.send_mail(
+                to:         site.mail,
+                from:       site.mail,
+                subject:    report[:titre],
+                message:    report[:message],
+                formated:   true
+            )
+            if res === true
+                log "    <- OK Rapport envoyé avec succès"
+            else
+                mess_err = ["# ERREUR EN TRANSMETTANT LE RAPPORT :"]
+                mess_err << res.message
+                mess_err += res.backtrace
+                mess_err = "    " + mess_err.join("\n    ")
+                log mess_err
+                log "    <- SiteHtml::Connexions::send_report"
+            end
+        end
+
+        # On consigne le rapport dans un fichier
+        def consigne_report
+            folder_rapports = "#{THIS_FOLDER}/rapports_connexions"
+            `mkdir -p #{folder_rapports}`
+            ntime = Time.now.strftime("%d_%m_%Y-%H-%M")
+            path_rapport = File.join(folder_rapports, "connexions-#{ntime}")
+            File.open(path_rapport, 'wb'){ |f|
+                f.puts report[:titre]
+                f.puts report[:message]
+            }
+        end
+
+        # À la fin de l'opération on peut détruire toutes les connexions
+        # actuelles pour repartir de zéro.
+        def remove_connexions
+            table_connexions_per_ip.delete(where: "time <= #{@start_time}")
+        end
+
+        # La table contenant les connexions provisoires
+        def table_connexions_per_ip
+            @table_connexions_per_ip ||= site.dbm_table(:hot, 'connexions_per_ip')
+        end
+
+        # ---------------------------------------------------------
+        # Méthodes fonctionnelles
+        # ---------------------------------------------------------
+
+        def recolte_connexions_by_ip
+            # On récupère toutes les connexions qui ont eu lieu depuis
+            # le dernier rapport.
+            all_connexions = table_connexions_per_ip.select
+            # On définit le temps de départ à maintenant pour supprimer
+            # toutes ces connexions en fin de rapport, si tout s'est
+            # bien passé
+            @start_time = Time.now.to_i
+            all_connexions.each do |con|
+                ip = con[:ip]
+                @report[:by_ip][ip] ||= {times: Array::new, routes: Array::new}
+                @report[:by_ip][ip][:times]   << con[:time]
+                @report[:by_ip][ip][:routes]  << con[:route]
+            end
+
         end
 
         # Retourne TRUE si des connexions ont été trouvées. Dans le
