cea558a564e040deed408f9d8fb6e972aebcc069
Philippe Perret <philippe.perret@yahoo.fr>
2016-07-12 11:07:45 +0200

    Some stuff (on cron)

--- CRON2/lib/procedure/send_mail_admin.rb
+++ CRON2/lib/procedure/send_mail_admin.rb
@@ -5,20 +5,20 @@
 class CRON2
 
 
-    SEND_REPORT_TO_ADMIN = true 
+    SEND_REPORT_TO_ADMIN = true
 
     # La méthode principale qui envoie le mail final à l'administrateur
     # si c'est nécessaire, c'est-à-dire si la constante SEND_MAIL est à
     # true et/ou qu'il s'est produit des erreurs au cours du cron
     def send_mail_admin
-        # TODO Il faudra voir comment se définit ce nombre d'erreur en 
+        # TODO Il faudra voir comment se définit ce nombre d'erreurs en
         # sachant que s'il est ici, c'est bon, c'est une variable de
         # l'instance singleton de CRON2 qui pourra être affectée par
         # tous les autres processus.
        @nombre_erreurs ||= 0
         # En fait il faut envoyer le rapport seulement si la constante
         # le demande et/ou s'il y a des erreurs. Donc si le nombre d
-        # 'erreurs est nil et qu'il ne faut pas envoyer le rapport, 
+        # 'erreurs est nil et qu'il ne faut pas envoyer le rapport,
         # alors on peut s'en retourner illico.
         return nil if !SEND_REPORT_TO_ADMIN && @nombre_erreurs == 0
         # Initialisation du rapport administrateur
@@ -31,15 +31,20 @@ class CRON2
                     'Une erreur s’est produite'
                 end
             areport << "<strong class='warning' style='display:block;margin:2em;border:1px solid red;font-size:13pt'>#{mess} au cours du dernier cron LOCAL…</strong>\n"
-            areport << File.open(logerrorfile,'rb'){|f| f.read.force_encoding('utf-8')}
+            areport << File.open(logerrorfile,'r'){|f| f.read.force_encoding('utf-8')}
         end
 
         # On ajoute toujours le rapport normal s'il faut un rapport
         # complet. Sinon, on indique juste un message simple
         if File.exist?(logfile)
-            areport << File.open(logfile,'rb'){|f| f.read.force_encoding('utf-8')}
+            areport << File.open(logfile,'r'){|f| f.read.force_encoding('utf-8')}
         end
 
+        if areport.nil_if_empty.nil?
+          # On signale le problème mais on envoie quand même le mail
+          # pour signaler la chose.
+          areport = "Bizarrement, il n'y a aucun texte à reporter."
+        end
 
         # On transforme les retours chariot en DIV et on définit la
         # taille de la police.
@@ -50,7 +55,7 @@ class CRON2
 
         # On envoie le rapport
         site.send_mail_to_admin(
-            subject:        "RAPPORT CRON LOCAL #{NOW.as_human_date(true, true, ' ')}",
+            subject:        "RAPPORT CRON #{NOW.as_human_date(true, true, ' ')}",
             message:        areport,
             formated:       true,
             force_offline:  true
