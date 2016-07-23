9190b2cfa62b2555b334e2204314af7e7e237a1d
Philippe Perret <philippe.perret@yahoo.fr>
2016-07-23 19:49:35 +0200

    Lien vers le quiz du scénodico (section)

--- objet/scenodico/lib/required/scenodico/helper.rb
+++ objet/scenodico/lib/required/scenodico/helper.rb
@@ -9,7 +9,7 @@ class Scenodico
   DATA_ONGLETS = {
     'Accueil'       => 'scenodico/home',
     'Dictionnaire'  => 'scenodico/list',
-    'Quiz'          => 'quiz/10/show?qdbr=biblio',
+    'Quiz'          => 'scenodico/quiz',
     'Recherche'     => 'scenodico/search',
     'Proposer'      => 'scenodico/proposer'
   }
@@ -23,10 +23,23 @@ class Scenodico
       if user.admin?
         donglets.merge!(
           'Nouveau'     => 'scenodico/edit',
-          '[Edit Quiz]' => 'quiz/10/edit?qdbr=biblio'
+          '[Edit Quiz]' => route_last_quiz('edit')
           )
       end
       return donglets
+    end
+
+    # Retourne la route qui conduit au dernier quiz sur le
+    # scénodico
+    def route_last_quiz for = 'show'
+      drequest = {
+        where: "groupe = 'scenodico'",
+        order: 'created_at DESC',
+        limit: 1,
+        colonnes: []
+      }
+      id_last = site.dbm_table('quiz_biblio', 'quiz').select(drequest).first[:id]
+      "quiz/#{id_last}/edit?qdbr=biblio"
     end
 
   end #/<<self
