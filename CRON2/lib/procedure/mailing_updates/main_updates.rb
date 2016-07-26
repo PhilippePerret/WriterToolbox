eabcc5fd68fd0887651afa5519d098f19c87b607
Philippe Perret <philippe.perret@yahoo.fr>
2016-07-26 06:59:30 +0200

    Correction bug parenthèse

--- CRON2/lib/procedure/mailing_updates/main_updates.rb
+++ CRON2/lib/procedure/mailing_updates/main_updates.rb
@@ -15,30 +15,33 @@ class CRON2
 
     class << self
       def send_mail
-        # Pas d'annonce si ça n'est pas l'heure
+        # Pas d'annonce si ça n'est pas l'heure (minuit)
         Time.now.hour == 0 || return
-        # Pas d'annonce si la dernière remonte à moins de 23 heures
+        
+        # Pas d'annonce si la dernière annonce remonte à moins de 23 heures
         # C'est une protection dans le cas où plusieurs cron seraient appelés
         # dans l'heure, ce qui arrive souvent quand on teste.
         # L'heure est également enregistré dans la table des dernières dates
         site.get_last_date(:mail_updates, 0) < (Time.now - 23*3600).to_i || return
 
         # Pas d'annonce s'il n'y a rien à annoncer
-        no_updates_today = last_updates.empty?
-        no_updates_this_week = last_week_updates.empty?
-        no_updates_no_samedi = no_updates_today && !samedi?
-        samedi_but_no_updates = samedi? && no_updates_this_week)
+        no_updates_today        = last_updates.empty?
+        no_updates_this_week    = last_week_updates.empty?
+        no_updates_no_samedi    = no_updates_today && !samedi?
+        samedi_but_no_updates   = samedi? && no_updates_this_week
+        
         (no_updates_no_samedi || samedi_but_no_updates ) && return
 
         # ----------------------------------------------
         # On procède à l'envoi des dernières actualités
         # ----------------------------------------------
-        annonce_last_updates
-
-        # On indique l'heure du dernier envoi lorsque tout s'est bien passé
-        site.set_last_date(:mail_updates)
+        if annonce_last_updates
+          # On indique l'heure du dernier envoi lorsque tout s'est bien passé
+          site.set_last_date(:mail_updates)
+        end
 
       end
+      
       def http
         @http ||= "http://www.laboiteaoutilsdelauteur.fr"
       end
