9dfc95d79be2ad0f889dbc7fc46ceed86bca6801
Philippe Perret <philippe.perret@yahoo.fr>
2016-07-27 16:52:30 +0200

    Message indiquant les explications à donner

--- objet/citation/edit.rb
+++ objet/citation/edit.rb
@@ -17,10 +17,10 @@ class << self
       id_new_citation = table_citations.insert( cdata )
       cdata.merge!(id: id_new_citation)
       param(citation: cdata.merge(id: id_new_citation))
-      flash "Création de la nouvelle citation opérée (##{id_new_citation})."
+      flash "Création de la nouvelle citation opérée (##{id_new_citation}).#{ajout_message_descriptions}"
     else
       table_citations.update(cdata[:id], cdata)
-      flash "Modification de la citation ##{cdata[:id]} enregistrée."
+      flash "Modification de la citation ##{cdata[:id]} enregistrée.#{ajout_message_descriptions}"
     end
 
     # On ajoute aux données la short_url (bitly)
@@ -33,6 +33,20 @@ class << self
 
   end
 
+  # Un message ajouté à la confirmation de l'enregistrement pour
+  # indiquer le nombre de citations qui ont une description et celles
+  # qui ne l'ont pas.
+  def ajout_message_descriptions
+    sans_desc     = table_citations.count(where: 'description IS NULL OR description = ""')
+    nombre_total  = table_citations.count
+    avec_desc     = nombre_total - sans_desc
+    "Nombre total de citations : #{nombre_total} - avec description : #{avec_desc} - reste à expliciter : #{sans_desc}".in_div(class: 'small')
+  end
+
+  def descrition_nil?
+    @description_was_nil
+  end
+
   def check_data
     cdata[:description] = cdata[:description].gsub(/[\r\n]/,'').gsub(/[\r\n]/,'').nil_if_empty
     cdata[:citation] = cdata[:citation].nil_if_empty
@@ -43,6 +57,12 @@ class << self
     cdata[:id] = cdata[:id].nil_if_empty
     cdata[:id].nil? || cdata[:id] = cdata[:id].to_i
     cdata[:bitly] = cdata[:bitly].nil_if_empty
+    @description_was_nil =
+      if cdata[:id].nil?
+        true
+      else
+        new(cdata[:id]).description == nil
+      end
   rescue Exception => e
     error e.message
   else
