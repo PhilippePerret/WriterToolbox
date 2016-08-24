46ff99009954616e87f219cc5ade6dd9b7efc987
Philippe Perret <philippe.perret@yahoo.fr>
2016-08-24 16:29:17 +0200

    Récupération des données de résultats des quiz UNAN
    Pour les mettre dans la nouvelle table.

--- lib/app/console_scripts/sandbox.rb
+++ lib/app/console_scripts/sandbox.rb
@@ -15,28 +15,12 @@
 def log str
   console.sub_log "#{str}<br>"
 end
-# 
-# def table_questions_new
-#   @table_questions_new ||= site.dbm_table(:quiz_unan, 'questions')
-# end
-#
-# table_questions_new.select.each do |hquestion|
-#   qid = hquestion[:id]
-#   log "Question ##{qid} #{hquestion[:question]} - #{hquestion[:type]}"
-#   # rl1 => 1rh0
-#   # rc1 => 1rv0
-#   old_type = hquestion[:type]
-#   type_c, type_a, type_q = old_type.split('')
-#   new_type_a =
-#     case type_a
-#     when 'c' then 'v'
-#     else 'h'
-#     end
-#   new_type = "#{type_q}#{type_c}#{new_type_a}0"
-#   log "#{old_type} -> #{new_type}"
-#   datan = {
-#     type:       new_type,
-#     updated_at: NOW
-#   }
-#   table_questions_new.update(qid, datan)
-# end
+
+
+def table_resultats_unan
+  @table_resultats_unan ||= site.dbm_table(:quiz_unan, 'resultats', online = true)
+end
+def table_works_for uid
+  tb_name = "unan_works_#{uid}"
+  site.dbm_table(:users_tables, tb_name, online = true)
+end
